60a6b66f2dc01364b65e8c1c8c289ddc08451c6c
SonnyZhan <fumingzhang@umass.edu>
2024-05-23 14:20:31 -0400

    player_class_made

--- main.ts
+++ main.ts
@@ -60,3 +60,97 @@ class Board {
         return this.terrain_layout[0];
     }
 }
+
+// enum differentcards_developement { //25 cards = 14 Soldier + 6 Progress + 5 Victory
+//     Soldier, Progress, Victory
+// }
+
+// function getrandomcards(){
+//     //randomizer 
+
+//     switch (differentcards_developement){   
+//         case :
+
+//         case ://card name 
+//         //function card
+//         case : 
+//     }
+// } 
+
+//card effect
+let deck = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3]; // 1 = knight, 2 = progress, 3 = victory
+
+class developement_Cards {
+    cardsInHand_developement: number[];
+    
+    constructor() {
+        this.cardsInHand_developement = [];
+    }
+
+    Drawhand_developement(nums: number) {
+        //num = number of cards we draw
+        //randomizer of numbers that represent cards
+        //push into the array
+
+        for(let i = 0, cards = 0; i < deck.length && cards <= nums; i++, cards++){
+            
+        }
+    }
+
+    removeCard(index: number): boolean {
+        if (index !== -1) {
+            this.cardsInHand_developement.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+
+    //usecard (arr: number[], index: number){
+        
+    // }
+
+
+
+    getCards(): number[] {
+        return this.cardsInHand_developement;
+    }
+}
+
+class Player{
+    resources: { [key in Resource]: number };
+    settlement_count: number;
+    roads_count: number;
+    roads_location: number[];
+
+
+    constructor() { //if player are in these terrain add to resources
+        this.resources = {
+            [Resource.brick]: 0,
+            [Resource.wood]: 0,
+            [Resource.ore]: 0,
+            [Resource.wheat]: 0,
+            [Resource.sheep]: 0,
+            [Resource.random]: 0,
+        };
+        
+        this.settlement_count = 0;
+        this.roads_count = 0;
+        this.roads_location = [];
+    
+    }
+
+    increment_resource(num: number, territorytype: Resource){
+        this.resources[territorytype] += num;
+    }
+
+    getResources_types(resourceType: Resource){ //see resource amounts
+        return this.resources[resourceType];
+    }
+
+    roadscounter(){ 
+        this.roads_count++;
+    }
+
+
+}
+
